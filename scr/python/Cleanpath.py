8f18fcb94865a3629f2505c77d22722cc9c2bc33
kristerklister <krister.laag@wtstime.se>
2012-11-08 12:40:25 +0100

    Fixed merge conflict, back to working one ; )

--- scr/python/Cleanpath.py
+++ scr/python/Cleanpath.py
@@ -40,7 +40,6 @@ def cleanPath(path):
 def cleanDirectoryPaths(rootDirectory):
 	for currentFile in glob.glob( os.path.join(rootDirectory, '*') ):
 		if os.path.isdir(currentFile):
-<<<<<<< HEAD
 			print "---> renamed currentDir: " + currentFile + " to: " + cleanPath(currentFile)	
 			os.rename(currentFile, cleanPath(currentFile))
 			print "---> renamed OK!"
@@ -53,17 +52,6 @@ def cleanPaths(rootDirectory):
 		print "---> renamed currentFile: " + currentFile + " to: " + cleanPath(currentFile)	
 		os.rename(currentFile, cleanPath(currentFile))
 		print "---> renamed OK!"
-=======
-			print "currentFileD:" + currentFile + ":" + cleanPath(currentFile)
-			#os.rename(currentFile, cleanPath(currentFile))
-			scandirs(cleanPath(currentFile))
-		myFiles.append(currentFile)
-		if os.path.isdir(currentFile):
-			print ""
-		else:
-			print "currentFile:" + currentFile + ":" + cleanPath(currentFile)
-			#os.rename(currentFile, cleanPath(currentFile))
->>>>>>> commented out renaming
 
 if __name__ == '__main__':
 	# Mikaels root dir
